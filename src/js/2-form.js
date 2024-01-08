ded3956602f802f21060b72f79d8da84f593cf76
Volodymyr <39493622+VolodymyrKozel@users.noreply.github.com>
2024-01-08 13:04:46 +0100

    v2

---
+++ src/js/2-form.js
@@ -0,0 +1,33 @@
+const form = document.querySelector('.feedback-form');
+const storageKey = 'feedback-form-state';
+const inputsData = {
+  email: '',
+  message: '',
+};
+addEventListener('load', () => {
+  if (localStorage.getItem(storageKey) !== null) {
+    const inputsData = JSON.parse(localStorage.getItem(storageKey));
+    form.elements.email.value = inputsData.email;
+    form.elements.message.value = inputsData.message;
+  }
+});
+
+const handleInput = e => {
+  inputsData.email = e.currentTarget.elements.email.value;
+  inputsData.message = e.currentTarget.elements.message.value;
+  inputsData.email = inputsData.email.trim();
+  inputsData.message = inputsData.message.trim();
+  localStorage.setItem(storageKey, JSON.stringify(inputsData));
+};
+const handleSubmit = e => {
+  e.preventDefault();
+  if (form.elements.email.value === '' || form.elements.message.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+  form.reset();
+  console.log(inputsData);
+  localStorage.removeItem(storageKey);
+};
+
+form.addEventListener('input', handleInput);
+form.addEventListener('submit', handleSubmit);
