72e02d6c92444253b24e6220ee362107b369cfc7
Volodymyr <39493622+VolodymyrKozel@users.noreply.github.com>
2024-02-07 15:47:23 +0100

    refactoring

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,31 +1,51 @@
 const form = document.querySelector('.feedback-form');
 const storageKey = 'feedback-form-state';
-const inputsData = {
-  email: '',
-  message: '',
-};
+const inputsData = { email: '', message: '' };
 addEventListener('load', () => {
-  if (localStorage.getItem(storageKey) !== null) {
-    const inputsData = JSON.parse(localStorage.getItem(storageKey));
-    form.elements.email.value = inputsData.email;
-    form.elements.message.value = inputsData.message;
-  }
+  restoreData();
 });
 
 const handleInput = e => {
   inputsData.email = e.currentTarget.elements.email.value.trim();
   inputsData.message = e.currentTarget.elements.message.value.trim();
-  localStorage.setItem(storageKey, JSON.stringify(inputsData));
+  saveToLS(storageKey, inputsData);
 };
 const handleSubmit = e => {
   e.preventDefault();
+  validateForm();
+  console.log(inputsData)
+  form.reset();
+  localStorage.removeItem(storageKey);
+};
+
+//helper functions
+function validateForm() {
   if (form.elements.email.value === '' || form.elements.message.value === '') {
     return alert('Please fill in all the fields!');
   }
-  form.reset();
-  console.log(inputsData);
-  localStorage.removeItem(storageKey);
-};
+}
+function restoreData() {
+  const inputsDataFromStore = loadFromLS(storageKey) || {
+    email: '',
+    message: '',
+  };
+  form.elements.email.value = inputsDataFromStore.email;
+  form.elements.message.value = inputsDataFromStore.message;
+}
+function loadFromLS(key = 'empty') {
+  const data = localStorage.getItem(key);
+  try {
+    const result = JSON.parse(data);
+    return result;
+  } catch {
+    return data;
+  }
+}
+
+function saveToLS(key, value) {
+  const jsonData = JSON.stringify(value);
+  localStorage.setItem(key, jsonData);
+}
 
 form.addEventListener('input', handleInput);
 form.addEventListener('submit', handleSubmit);
